265905e7da7aa69d8d748532648f694c9408e909
Jp0Martins <148448718+Jp0Martins@users.noreply.github.com>
2026-05-19 19:34:32 -0300

    Delete frontend directory

--- frontend/js/api-config.js
+++
@@ -1,3 +0,0 @@
-window.API_URL = "http://localhost:8000/api/consumos";       // Backend Python - Consumos
-window.SETORES_API = "http://localhost:8080/api/setores";    // Backend Java - Setores
-window.METAS_API = "http://localhost:8000/api/metas";        // Backend Python - Metas
