6ccdea5103de9fa383ffe625e2c5609ae2daa001
Jp0Martins <148448718+Jp0Martins@users.noreply.github.com>
2026-05-19 19:34:43 -0300

    Delete backend directory

--- backend/src/main/java/com/ecogestor/repository/EmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecogestor.repository;
-
-import com.ecogestor.model.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, UUID> {}
